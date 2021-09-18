775960aaffcef8c4c78babc1beab59ea0050725c
Divine <dchelladurai@vmware.com>
2021-09-18 14:34:04 +0530

    initial commit

---
+++ app/src/main/java/com/xzf/droidbottomnav/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.xzf.droidbottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view);
+        //val navController = findNavController(R.id.fragmentContainerView)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+
+        val appBarConfiguration = AppBarConfiguration((setOf(R.id.firstFragment,R.id.secondFragment,R.id.thirdFragment)))
+
+        setupActionBarWithNavController(navController,appBarConfiguration);
+
+        bottomNavigationView.setupWithNavController(navController);
+    }
+}
